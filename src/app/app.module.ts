a9e1acca2b45bfd6b92802b707c5068994b8e6f8
SebastianAguirre17 <sebaaguirre2012@gmail.com>
2020-10-07 20:13:07 -0300

    header + footer + servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        AboutComponent,
+        ItemComponent,
+        PortafolioComponent
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [
+
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
